9ba7a18a46b7cfa47c106e742deb21fc5dc26ca3
Ryan Volz <ryan.volz@gmail.com>
2025-02-14 17:27:04 -0500

    Working gnuradio-leo recipe and skeleton for gnuradio-oot-forge

---
+++ recipes/leo/cache_build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [[ $target_platform == osx* ]] ; then
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+# Workaround for no std::aligned_alloc with osx-64
+# https://github.com/chriskohlhoff/asio/issues/1090
+# Maybe remove when boost is updated to 1.80.0?
+if [[ "${target_platform}" == "osx-64" ]]; then
+  export CXXFLAGS="-DBOOST_ASIO_DISABLE_STD_ALIGNED_ALLOC ${CXXFLAGS}"
+fi
+
+cmake -E make_directory buildconda
+cd buildconda
+
+cmake_config_args=(
+    -DCPM_sgp4_SOURCE="$SRC_DIR/sgp4"
+    -DLIB_SUFFIX=""
+    -DENABLE_DOXYGEN=OFF
+    -DENABLE_TESTING=ON
+)
+
+cmake ${CMAKE_ARGS} -G "Ninja" .. "${cmake_config_args[@]}"
+cmake --build . --config Release -- -j${CPU_COUNT}
+cmake --build . --config Release --target install
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    ctest --build-config Release --output-on-failure --timeout 120 -j${CPU_COUNT}
+fi
