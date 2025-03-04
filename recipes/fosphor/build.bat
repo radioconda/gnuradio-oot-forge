23675fdc24aeda5db6773acbf373a45767970e5d
Ryan Volz <ryan.volz@gmail.com>
2025-03-04 16:20:59 -0500

    Add fosphor (#15)
    * Add render-variants to pre-commit
    
    * Add fosphor
    
    * Update upstream to fix typo
    
    * Remove fmt dependency
    
    * Ensure desired components are enabled
    
    * Update version to relax how libpng is found
    
    * Add zlib to host so libpng can be found
    
    * Update with upstream change to modern GR logging
    
    * Another try at fixing logging for Windows build
    
    * Another try to fix logging on Windows

---
+++ recipes/fosphor/build.bat
@@ -0,0 +1,33 @@
+setlocal EnableDelayedExpansion
+@echo on
+
+:: Make a build folder and change to it
+cmake -E make_directory buildconda
+cd buildconda
+
+:: configure
+cmake -G "Ninja" %CMAKE_ARGS% ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" ^
+    -DCMAKE_PREFIX_PATH="%PREFIX%\Library" ^
+    -DGR_PYTHON_DIR:PATH="%SP_DIR%" ^
+    -DENABLE_DOXYGEN=OFF ^
+    -DENABLE_GLFW=ON ^
+    -DENABLE_PNG=ON ^
+    -DENABLE_PYTHON=ON ^
+    -DENABLE_QT=ON ^
+    -DENABLE_TESTING=ON ^
+    -DGLFW3_LIBRARIES="%LIBRARY_LIB%\glfw3dll.lib" ^
+    ..
+if errorlevel 1 exit 1
+
+:: build
+cmake --build . --config Release -- -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+:: install
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+:: test
+ctest --build-config Release --output-on-failure --timeout 120 -j%CPU_COUNT%
+if errorlevel 1 exit 1
